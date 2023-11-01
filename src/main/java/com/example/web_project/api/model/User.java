0e35056502235e05d97c6d083884af20503063b4
Lena D <dodelena@gmail.com>
2023-11-01 22:16:12 +0100

    [DEV] - LD : Creation of User class and link between database and application made

---
+++ src/main/java/com/example/web_project/api/model/User.java
@@ -0,0 +1,29 @@
+package com.example.web_project.api.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "users")
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_user;
+
+    @Column(name = "login", nullable = false, length = 30)
+    private String login;
+
+    @Column(name = "first_name", nullable = false, length = 30)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 30)
+    private String lastName;
+
+}
---
+++ src/main/java/com/example/web_project/service/UserService.java
@@ -0,0 +1,24 @@
+package com.example.web_project.service;
+/*
+import com.example.web_project.api.model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+
+    //Here the service connects to the DB
+
+    //Temporary
+    private List<User> userList;
+
+    public UserService(){
+        userList = new ArrayList<>();
+    }
+
+    public User getUser(Long id) {
+
+    }
+}*/
